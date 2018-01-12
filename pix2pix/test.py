2ca0321375adec2b407855beffb4a234eb73bed9
chipyaya <b03902024@ntu.edu.tw>
2018-01-13 02:05:02 +0800

    add test code

---
+++ pix2pix/test.py
@@ -0,0 +1,38 @@
+import time
+import os
+from options.test_options import TestOptions
+from data.data_loader import CreateDataLoader
+from models.models import create_model
+
+opt = TestOptions().parse()
+opt.nThreads = 1   # test code only supports nThreads = 1
+opt.batchSize = 1  # test code only supports batchSize = 1
+opt.serial_batches = True  # no shuffle
+opt.no_flip = True  # no flip
+
+data_loader = CreateDataLoader(opt)
+dataset = data_loader.load_data()
+model = create_model(opt)
+# test
+
+for j,data in enumerate(dataset):
+    model.set_input(data)
+    model.test()
+    visuals = model.get_current_visuals()
+    # Combine 100 image into one
+    for label, image_numpy in visuals.items():
+        if (j+1) % 10 == 1:
+            img_comb_row[label] = image_numpy
+        else:
+            img_comb_row[label] = np.concatenate([img_comb_row[label], image_numpy], 1)
+
+        if j == 9:
+            img_comb[label] = img_comb_row[label]
+        elif (j+1) % 10 == 0:
+            img_comb[label] = np.concatenate([img_comb[label], img_comb_row[label]], 0)
+
+# Save
+for label, image_numpy in img_comb.items():
+    image_name = '%s_%s.png' % (epoch, label)
+    save_path = os.path.join(opt.testing_path, image_name)
+    util.save_image(image_numpy, save_path)
